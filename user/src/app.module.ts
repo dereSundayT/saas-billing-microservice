cfd22553007c5910b5db2a82ff748799a09b9dbd
dereSundayT <toluwanisundaydere@gmail.com>
2025-03-05 23:22:31 +0100

    dokerized user service

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // Makes ConfigModule available everywhere without re-importing
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('MYSQL_HOST'),
+                port: configService.get<number>('MYSQL_PORT'),
+                database: configService.get<string>('MYSQL_DATABASE'),
+                username: configService.get<string>('MYSQL_USERNAME'),
+                password: configService.get<string>('MYSQL_PASSWORD'),
+                entities: [],
+                synchronize: true,
+            }),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
