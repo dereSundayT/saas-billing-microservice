8f18472da6d5c89af97fa642566a6a20dc9c3241
dereSundayT <toluwanisundaydere@gmail.com>
2025-03-06 01:50:03 +0100

    feat: added nats servier to docker compose file

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.NATS,
+        options: {
+            // servers: ['nats://localhost:4222'],
+            servers: ['nats://nats:4222'],
+        },
+    });
+
+    await app.listen();
+
 }
+
 bootstrap();
